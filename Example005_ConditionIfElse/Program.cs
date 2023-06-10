8399b4f8d1b92b9e422641a59ace75e65e699e80
Irina <irina@Mac-mini-Irina.local>
2023-06-10 23:56:05 +0500

    add Example 005 Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "irina")
+{
+    Console.WriteLine("YEEE, It's Irina!!!");
+}
+
+else
+{
+    Console.Write("Hello, ");
+    Console.Write(username);
+    Console.Write("!");
+}
