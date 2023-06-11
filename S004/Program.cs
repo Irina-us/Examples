b30dca28ceb820a65e36973274f4b289e52154c5
Irina <irina@Mac-mini-Irina.local>
2023-06-11 20:05:51 +0500

    Семинар, пример4

---
+++ S004/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 5 Напишите программу, которая на вход принимает одно число (N),
+// а на выходе показывает числа в промжутке от -N до N
+// 2 -> "-2,-1, 0, 1, 2"
+
+Console.Clear();
+Console.WriteLine("Показывает числа в промежутке от -N до N");
+
+System.Console.WriteLine("Введите любое число");
+int N = Convert.ToInt32( Console.ReadLine() );
+int start = -N;
+while (start != N)
+{
+    Console.Write(start + " ");
+    start += 1;
+}
