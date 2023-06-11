053ced1f6df517a50d53a3794b242143f0de4522
Irina <irina@Mac-mini-Irina.local>
2023-06-11 20:11:35 +0500

    Семинар, пример3

--- S003/Program.cs
+++ S003/Program.cs
@@ -10,18 +10,17 @@ int num = Convert.ToInt32( Console.ReadLine() );
 
 if (num == 1)
   Console.WriteLine(num+" - Понедельник");
-if (num == 2)
-  Console.WriteLine(num+" - Вторник");
-if (num == 3)
-  Console.WriteLine(num+" - Среда");
-if (num == 4)
-  Console.WriteLine(num+" - Четверг");
-if (num == 5)
-  Console.WriteLine(num+" - Пятница");
-if (num == 6)
-  Console.WriteLine(num+" - Суббота");
-if (num == 7)
-  Console.WriteLine(num+" - Воскресенье");
-
+    else if (num == 2)
+        Console.WriteLine(num+" - Вторник");
+    else if  (num == 3)
+        Console.WriteLine(num+" - Среда");
+    else if  (num == 4)
+        Console.WriteLine(num+" - Четверг");
+    else if  (num == 5)
+        Console.WriteLine(num+" - Пятница");
+    else if  (num == 6)
+        Console.WriteLine(num+" - Суббота");
+    else if  (num == 7)
+        Console.WriteLine(num+" - Воскресенье");
 else
     Console.WriteLine("В неделе 7 дней, введите корректное число!");
