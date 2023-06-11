dde89e35afdb817761631faf923d4bc85d241c32
Irina <irina@Mac-mini-Irina.local>
2023-06-11 18:58:15 +0500

    Семинар, пример3.2

---
+++ S003_vers2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.Clear();
+Console.WriteLine("Определяем день недели");
+
+System.Console.WriteLine("Введите номер дня недели от 1 до 7");
+int num = Convert.ToInt32( Console.ReadLine() );
+if (num == 7)
+{
+    num = 0;
+}
+    Console.WriteLine((DayOfWeek)num);
